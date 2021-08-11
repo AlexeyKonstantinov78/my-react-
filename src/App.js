149c828544b5d2b8401a0795e34d5a73e4b85c32
Aleksey Konstantinov <alex_kav@mail.ru>
2021-08-11 15:31:34 +0300

    управляемый компонент

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Counter from "./components/Counter";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import './styles/App.css';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from "./components/UI/input/MyInput";
 
 
 function App() {
@@ -13,14 +15,11 @@ function App() {
     { id: 3, title: 'Js3', body: 'Description' },
   ])
 
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: 'poeton', body: 'Description' },
-    { id: 2, title: 'poeton', body: 'Description' },
-    { id: 3, title: 'poeton', body: 'Description' },
-  ])
 
   const [likes, setLikes] = useState(0),
     [value, setValue] = useState('текст');
+
+  const [title, setTitle] = useState('');
 
   function increment() {
     setLikes(likes + 1);
@@ -30,12 +29,29 @@ function App() {
     setLikes(likes - 1);
   }
 
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+  }
+
   return (
     <div className="App">
       <Counter />
-      {/* <PostItem post={} /> */}
+      {/* управляемый компонент */}
+
+      <form>
+        <MyInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text"
+          placeholder="Название поста" />
+        <MyInput type="text" placeholder="Описание поста" />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+
       <PostList posts={posts} title='Список постовJS' />
-      <PostList posts={posts2} title='Список постов Poeton' />
+
+      {/* <PostItem post={} /> */}
 
       {/* <h2>{value}</h2>
       <input
